611fb43a9b9ff0b7550bcce28f27c5f5e3da78e7
Michael Carlson <michael@brainrush.io>
2020-06-04 14:14:52 -0700

    added gradient color and actuals

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.get('/comparison', (req, res) => {
   res.sendFile(path.join(__dirname + '/comparison.html'));
 })
 
+app.get('/comparison2', (req, res) => {
+  console.log("request at /comparison");
+  res.sendFile(path.join(__dirname + '/comparisonpreset.html'));
+})
+
 app.get('/repop/old', (req, res) => {
   console.log("request at /repop");
   function populateAllData () {
