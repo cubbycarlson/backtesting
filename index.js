3d49676162f06d984bd1a66e68559db236066078
Michael Carlson <cubbycarlson@gmail.com>
2020-06-23 15:23:59 -0700

    moved old files

--- index.js
+++ index.js
@@ -15,46 +15,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.get('/old', (req, res) => {
-  console.log("request at /");
-  res.sendFile(path.join(__dirname + '/old.html'));
-})
-
-app.get('/comparison', (req, res) => {
-  console.log("request at /comparison");
-  res.sendFile(path.join(__dirname + '/comparison.html'));
-})
-
-app.get('/comparison1', (req, res) => {
-  console.log("request at /comparison1");
-  res.redirect('/comparison')
-})
-
-app.get('/comparison2', (req, res) => {
-  console.log("request at /comparison");
-  res.sendFile(path.join(__dirname + '/comparisonpreset.html'));
-})
-
-app.get('/comparison3', (req, res) => {
-  console.log("request at /comparison");
-  res.sendFile(path.join(__dirname + '/comparisonpreset2.html'));
-})
-
-app.get('/comparison/ihme', (req, res) => {
-  console.log("request at /comparison/ihme");
-  res.sendFile(path.join(__dirname + '/ihme.html'));
-})
-
-app.get('/comparison/ihme2', (req, res) => {
-  console.log("request at /comparison/ihme2");
-  res.sendFile(path.join(__dirname + '/ihme2.html'));
-})
-
-app.get('/comparison/cdc', (req, res) => {
-  console.log("request at /comparison/cdc");
-  res.sendFile(path.join(__dirname + '/cdc.html'));
-})
-
 app.get('/repop/old', (req, res) => {
   console.log("request at /repop");
   function populateAllData () {
@@ -86,32 +46,10 @@ app.get('/repop/usa', (req, res) => {
   // res.sendFile(path.join(__dirname + '/html/index.html'));
 })
 
-app.get('/comparison/ihme/table', (req, res) => {
-  res.sendFile(path.join(__dirname + '/ihmetable.html'));
-})
-
 app.get('/pull/actual', (req, res) => {
   getActualData();
   res.redirect("/");
 })
-
-app.get('/all', (req, res) => {
-  res.sendFile(path.join(__dirname + '/allTable.html'));
-})
-
-app.get('/beta', (req, res) => {
-  res.sendFile(path.join(__dirname + '/beta.html'));
-});
-
-app.get('/test', (req, res) => {
-  res.sendFile(path.join(__dirname + '/test.html'));
-});
-
-app.get("/tooltip", (req,res) => {
-  res.sendFile(path.join(__dirname + '/tooltip.html'));
-
-})
-
 
 app.listen(process.env.PORT || 9999, () => {
   console.log("listening...");
